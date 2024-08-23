40168a9f83bce689c7a88e3b70ba85a7a7fc66cf
Utkarsh Jaiswal <utkarsh.jaiswal0004@gmail.com>
2024-08-23 20:52:37 +0530

    Handle single and multiple numbers separated by commas

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,2 +1,8 @@
 def add(numbers: str) -> int:
     return 0
+
+def add(numbers: str) -> int:
+    if not numbers:
+        return 0
+    num_list = map(int, numbers.split(","))
+    return sum(num_list)
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -3,3 +3,9 @@ from src.string_calculator import add
 
 def test_empty_string():
     assert add("") == 0
+    
+def test_single_number():
+    assert add("1") == 1
+
+def test_two_numbers():
+    assert add("1,2") == 3
