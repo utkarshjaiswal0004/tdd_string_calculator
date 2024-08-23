a656ec5dbd6c436072bde52b76e3c01122fe1712
Utkarsh Jaiswal <utkarsh.jaiswal0004@gmail.com>
2024-08-23 20:55:16 +0530

    Final refactor for add function in String Calculator

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -9,17 +9,19 @@ def add(numbers: str) -> int:
     num_list = map(int, numbers.split(","))
     return sum(num_list)
 
+
 def add(numbers: str) -> int:
     if not numbers:
         return 0
 
+    delimiter = ","
     if numbers.startswith("//"):
         delimiter, numbers = numbers[2:].split("\n", 1)
-        numbers = numbers.replace(delimiter, ",")
-    else:
-        numbers = numbers.replace("\n", ",")
 
-    num_list = list(map(int, numbers.split(",")))
+    # Normalize delimiters to comma
+    numbers = numbers.replace("\n", delimiter)
+    
+    num_list = list(map(int, numbers.split(delimiter)))
 
     # Check for negative numbers
     negatives = [num for num in num_list if num < 0]
